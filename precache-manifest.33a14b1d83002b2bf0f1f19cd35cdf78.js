5f406cdf934385ee890cd091e79dcc5d67a1090a
beathan <beathan.n@gmail.com>
2019-01-20 22:10:54 -0600

    Updates

---
+++ precache-manifest.33a14b1d83002b2bf0f1f19cd35cdf78.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "39770352af801d4d0269",
+    "url": "/beerbank/static/css/main.7b221108.chunk.css"
+  },
+  {
+    "revision": "39770352af801d4d0269",
+    "url": "/beerbank/static/js/main.39770352.chunk.js"
+  },
+  {
+    "revision": "e5702357447a8e2bd3a6",
+    "url": "/beerbank/static/css/1.cc5e5f9d.chunk.css"
+  },
+  {
+    "revision": "e5702357447a8e2bd3a6",
+    "url": "/beerbank/static/js/1.e5702357.chunk.js"
+  },
+  {
+    "revision": "75f20faceaf8cf4eacae",
+    "url": "/beerbank/static/js/runtime~main.75f20fac.js"
+  },
+  {
+    "revision": "9d3acab1573019d184d00215cbf7978e",
+    "url": "/beerbank/static/media/star.9d3acab1.svg"
+  },
+  {
+    "revision": "83429d06236818a1676bf06a3e1a5baa",
+    "url": "/beerbank/static/media/star-filled.83429d06.svg"
+  },
+  {
+    "revision": "d375d696297f35cf72d62a6c3ddbd698",
+    "url": "/beerbank/static/media/close.d375d696.svg"
+  },
+  {
+    "revision": "c887bd7d806827c3c6ea871fad034635",
+    "url": "/beerbank/index.html"
+  }
+];
--- src/Utils/findElementIndex.js
+++
@@ -1,5 +0,0 @@
-export function findElementIndex(needle, array) {
-    return array.findIndex((element) => {
-        return (element === needle);
-    })
-}
